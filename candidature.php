a090414c889835a1fb0fa4e91c9a660e5385cc0d
Rachad ADEKAMBI <adekambirachad@gmail.com>
2021-06-08 16:56:36 +0000

    update candidature.php

--- candidature.php
+++ candidature.php
@@ -1,56 +1,54 @@
 <?php
-
-//login to database
-if (isset($_POST["submit"])) {
-
-        $serveur = "localhost";
-        $login = "root";
-        $pas = "";
-                echo("iixii");
-      /*
-        $first_name = $_POST["first-name"];
-        $middle_name = $_POST["middle-name"];
-        $last_name = $_POST["last-name"];
-        $adress = $_POST["adress"];
-        $adress_plus = $_POST["adress-plus"];
-        $city = $_POST["city"];
-        $zip = $_POST["zip"];
-        $email = $_POST["email"];
-        $phone_code = $_POST["phone-code"];
-        $phone = $_POST["phone"];
-        $zip = $_POST["zip"];
-        $job = $_POST["job"];
-        $contract = $_POST["contract"];
-        $availability_date = $_POST["availability-date"];
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=trtp;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 
 
-        $connexion=new PDO("mysql:host=$serveur;dbname=trtp", $login, $pas);
-        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        $req = $connexion->prepare("INSERT INTO jobs
-         (date_inscription, first_name ,second_name, last_name, adress, city, zip, email,
-         phone_code, phone, job, contrat, availability_date)VALUES (NOW(), :first_name, 
-         :middle_name, :last_name, :adress, :city, :zip, :email, :phone_code, :phone, :job,
-         :contrat, :availability_date)
+$req = $bdd->prepare('INSERT INTO jobs
+(date_inscription, first_name ,second_name, last_name,
+ adress, city, zip, email, phone_code, phone, job, 
+ contrat, availability_date)');
 
 
-$req->bindParam(':first_name', $first_name);
-$req->bindParam(':middle_name', $middle_name);
-$req->bindParam(':last_name', $last_name);
-$req->bindParam(':adress', $adress);
-$req->bindParam(':city', $city);
-$req->bindParam(':zip', $zip);
-$req->bindParam(':email', $email);
-$req->bindParam(':phone_code', $phone_code);
-$req->bindParam(':phone', $phone);
-$req->bindParam(':job', $job);
-$req->bindParam(':contrat', $contract);
-$req->bindParam(':availability_date', $availability_date);
+$first_name = $_POST["first-name"];
+	$middle_name = $_POST["middle-name"];
+	$last_name = $_POST["last-name"];
+	$adress = $_POST["adress"];
+	$adress_plus = $_POST["adress-plus"];
+	$city = $_POST["city"];
+	$zip = $_POST["zip"];
+	$email = $_POST["email"];
+	$phone_code = $_POST["phone-code"];
+	$phone = $_POST["phone"];
+	$zip = $_POST["zip"];
+	$job = $_POST["job"];
+	$contract = $_POST["contract"];
+	$availability_date = $_POST["availability-date"];   
 
 
-$req->execute();
+$req->execute(array(
 
-echo:"envoye";
-         }
-         */
+	
+
+
+	'date_inscription' => NOW(),
+	'first_name' => $first_name,
+	'second_name' => $second_name,
+	'last_name' => $last_name,
+	'adress' => $adress,
+	'city' => $city,
+	'zip' => $zip,
+	'email' => $email,
+	'phone_code' => $phone_code,
+	'phone' => $phone,
+	'job' => $job,
+	'contrat' => $contract,
+	'availability_date' => $availability_date));
+
+	echo 'Le jeu a bien été ajouté !';
 ?>
